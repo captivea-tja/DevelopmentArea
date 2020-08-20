4f286d2246e2ad239017c062f9f70baf40c5519f
Timothy James <52675656+captivea-tja@users.noreply.github.com>
2020-08-20 16:24:55 -0400

    Update account_controller.py

--- cap_portal/controllers/account_controller.py
+++ cap_portal/controllers/account_controller.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 import base64
+import logging
 from odoo import fields as odoo_fields, http, tools, _, SUPERUSER_ID
 from odoo.exceptions import ValidationError, AccessError, MissingError, UserError
 from odoo.http import content_disposition, Controller, request, route
 from odoo.tools import consteq
 
 from odoo.addons.portal.controllers.portal import CustomerPortal
+
+_logger = logging.getLogger(__name__)
 
 CustomerPortal.OPTIONAL_BILLING_FIELDS.append('x_test_file')
 
@@ -17,9 +20,12 @@ class CustomCustomerPortal(CustomerPortal):
         
         res = super(CustomCustomerPortal, self).account(redirect=None, **post)
         
-        content = post.get('content')
-        FileData = content.read()
-        file_base64 = base64.encodestring(FileData)
+        for p in post:
+            _logger.info(p)
+        
+        #content = post.get('content')
+        #FileData = content.read()
+        #file_base64 = base64.encodestring(FileData)
         
         #test_file = post.get('x_test_file')
         #file_base64 = base64.encodestring(test_file.read())
